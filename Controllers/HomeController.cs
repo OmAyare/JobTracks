6135b65be7ba8fcb692629cb8c305bd63b48e573
HOME <HOME@DESKTOP-36GF5CB>
2025-07-11 22:07:52 +0530

    remove non reuired code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,33 +18,6 @@ namespace JobTracks.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Index(User adlo)
-        //{
-        //    var username = adlo.Username?.Trim().ToLower();
-        //    var password = adlo.Password?.Trim();
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        var us = db.Users.SingleOrDefault(x => x.Username == adlo.Username && x.Password == adlo.Password);
-        //        if (us != null)
-        //        {
-        //            Session["UserId"] = us.User_id;
-        //            Session["Username"] = us.Username;
-        //            Session["RoleId"] = us.Role_id;
-
-        //            ViewBag.ShowAnimation = true;
-
-        //            ViewBag.RoleId = us.Role_id;
-
-        //            return View(adlo); 
-        //        }
-        //    }
-
-        //    ViewBag.Error = "Invalid username or password.";
-        //    return View(adlo);
-        //}
-
         [HttpPost]
         public ActionResult Index(Login adlo)
         {
@@ -64,16 +37,7 @@ namespace JobTracks.Controllers
 
                     ViewBag.ShowAnimation = true;
                     ViewBag.RoleId = us.Role_id;
-                    // Redirect based on role
-                    //switch (us.Role_id)
-                    //{
-                    //    case 1:
-                    //        return RedirectToAction("Dashboard", "Admin", new { area = "Admin" });
-                    //    case 2:
-                    //        return RedirectToAction("Dashboard", "TeamLeader", new { area = "TeamLeader" });
-                    //    case 3:
-                    //        return RedirectToAction("Dashboard", "Recruiter", new { area = "Recruiter" });
-                    //}
+                   
                     return View(adlo);
                 }
 
@@ -83,11 +47,10 @@ namespace JobTracks.Controllers
             {
                 var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage);
                 ViewBag.Error = string.Join("<br/>", errors);
+                return View("NotFound", "Error");
             }
 
             return View(adlo);
         }
-
-
     }
 }
